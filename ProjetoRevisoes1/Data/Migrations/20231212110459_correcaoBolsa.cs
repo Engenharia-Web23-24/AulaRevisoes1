b0b716362b41ef9da37eb578251fa561998f9989
lfbutad <lfb@utad.pt>
2023-12-12 11:05:32 +0000

    Correção erro modelo dados.

---
+++ ProjetoRevisoes1/Data/Migrations/20231212110459_correcaoBolsa.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjetoRevisoes1.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class correcaoBolsa : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RegistadoEm",
+                table: "Bolsas",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RegistadoPor",
+                table: "Bolsas",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RegistadoEm",
+                table: "Bolsas");
+
+            migrationBuilder.DropColumn(
+                name: "RegistadoPor",
+                table: "Bolsas");
+        }
+    }
+}
